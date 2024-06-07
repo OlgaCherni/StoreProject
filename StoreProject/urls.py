2d2a0b5a842c4f42bd7b4f7f027af4337a6ec3ad
OlgaCherni <ok20com@hotmail.com>
2024-06-07 22:32:24 +0300

    adding applications and files

--- StoreProject/urls.py
+++ StoreProject/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for StoreProject project.
+URL configuration for app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,9 +15,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.conf.urls.static import static
+
+from StoreProject import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main')),                # доб пути из приложения main
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog')),
+    path('user/', include('users.urls', namespace='user')),
+    path('cart/', include('carts.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
 ]
+
+# if settings.DEBUG:
+#     urlpatterns += [
+#         path("__debug__/", include("debug_toolbar.urls")),
+#     ]
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
+
+
+"""
+www.site.com/admin/
+www.site.com
+www.site.com/about/
+www.site.com/catalog/
+www.site.com/catalog/product
+"""
