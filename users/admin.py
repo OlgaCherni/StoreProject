0596b9629e7c95e381848dc3d5746ebde0300b87
OlgaCherni <ok20com@hotmail.com>
2024-06-11 15:00:38 +0300

    Added applications with orders and shopping cart. Uploaded the picture.

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from carts.admin import CartTabAdmin
 from orders.admin import OrderTabulareAdmin
-
 from users.models import User
 
 # admin.site.register(User)
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
